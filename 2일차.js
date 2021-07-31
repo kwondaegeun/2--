aeda83a6736c2f20cfa9bbf61d580b2532d6f16b
kwondaegeun111 <kwondaegeun111@gmail.com>
2021-07-31 10:31:19 +0900

    first commit

---
+++ 2일차.js
@@ -0,0 +1,167 @@
+console.log(Astring > Bstring);
+let A = 10;
+let B = 20;
+
+console.log(A > B);
+console.log(A == B);
+
+let Asting = 'ABC';
+let Bsting = 'ABD';
+
+console.log(Asring > Bstring);
+
+let D = '2'
+let E = 2;
+
+console.log(D != E)
+
+console.log(0 == false);
+console.log('' == false);
+console.log(null === undefined);
+
+let kim;
+
+
+<
+>
+===
+==
+
+let kim = "kim";
+let lee = "lee";
+
+lee = "kim";
+
+if (lee === kim)
+    console.log('같아');
+}
+else if (lee == "lee") {
+    console.log('중복이야');
+
+    // 19살 성인 남자면 출입 불가능
+    // 19살 성인 여자면 출입 가능
+    // && and
+    // or
+    let age = 16;
+    let gender = "남자";
+
+    let message = (age < 19 && gender === "남자") ? "청소년 이용 불가입니다" : "성인 안녕"
+    console.log(message) 청소년 이용 불가입니다
+
+    and && , or ||
+        let gender = "남자";
+    let age = 11;
+
+    if
+    if (성별이 남자이고 10 살이상인사람) {
+        i is not defined
+        console.log("꼬맹이");
+    } else if (성별이 남자이고 10 살 미만인사람)
+        console.log("애기");
+    else
+        console.log("아무거나");
+
+
+    if (gender === "남자" && age >= 10)
+        console.log("꼬맹이");
+    else if (gender === "남자" && age < 10)
+        console.log("애기");
+    else console.log("아무거나");
+
+    var name = "";
+
+    if (Name == "") {
+        Name = "kim"
+        consloe.log(Name);
+        kim
+    }
+
+    let height;
+    // if (height === undefined){
+    //     height =100;
+
+    //}
+    height = height ? ? 100;
+    consloe.log(height);
+    100
+
+    let count = 0;
+    //let trigger = false;
+    // for while 문
+    while (true) {
+        if (count == 10)
+            console.log('count가 10이 되었습니다')
+        continue;
+    }
+
+    if (count == 15)
+
+        console.log(count);
+
+    for (let i = 1; i < 10; i++) {
+        console.log(i) 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    }
+    // continue, 
+    console.log(count);
+
+    for (;;) {
+
+
+    }
+
+    // *
+    // **
+    // ***
+    // ****
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 1; j <= i; j++)
+            console.log('*');*, *, *, *, *
+        conslole.log('');
+        [empty string], [empty string], [empty sring]
+    }
+
+    for (let i = 1; i <= 4; i++) {
+        let star = "";
+        for (let j = 0; j < i; j++) {
+            star += "*";
+        }
+        console.log(star);*, ** * , ** ** ** , ** ** ** ** **
+    }
+
+    let age = 30;
+
+    switch (age) {
+        case "explo":
+        case "chrome":
+            console.log("구데기")
+        case "safari"
+        console.log("맥만 좋음")
+        case "fireFox"
+        consloe.log("캐릭터 귀여움")
+
+        default:
+            console.log("그런 브라우저가 세상있다니!")
+    }
+    let AAA = 10;
+
+    function add(a, ) {
+        let AA = 20;
+        console.log(a + );
+        30
+        return a = ;
+    }
+    console.log(AA) 20
+    let result = add(10, 20);
+    console.log(result) 30
+
+
+    for(int i=0;i<4;i++){
+        for(int j=0;j<3-i;j++){
+            System.out.print(" ");
+        }
+        for(int j=0;j<2*i+1;j++){
+            System.out.print("*");
+        }
+        System.out.prinln("");
+    }
